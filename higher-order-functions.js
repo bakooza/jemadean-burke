97df4ecd592977e7f834918217fd11b49a0fef61
Jemadean Dobrosielski <jemadean@gmail.com>
2018-07-17 13:21:03 -0700

    added turtlesteps

--- higher-order-functions.js
+++ higher-order-functions.js
@@ -65,3 +65,25 @@ const rocksWarning = hazardWarningCreator('Rocks on the Road');
 const hurricaneWarning = hazardWarningCreator('Hurricane coming for you');
 const tornadoWarning = hazardWarningCreator('Tornado near');
 
+
+// // A turtle's movements can be represented by an array which looks like this: [3, 4]. The first item in the 
+// array represents the number of steps the turtle takes forwards. The second number in the array is the number 
+// of steps the turtle takes to the left.
+// // Here is an array of different movements made by a turtle: 
+// [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]].
+// // Use the filter method to remove any items where the turtle moves backwards or to the right 
+// (i.e. where either the first of second number is an item is negative).
+// // Use the map method to create a new array containing how many steps the turtle makes in total with 
+// each movement (i.e. the first and second number added together).
+// // Use the forEach method to log out how many steps the turtle took in each case.
+
+//const result = words.filter(word => word.length > 6)
+
+const turtleSteps = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+
+const Result = turtleSteps
+                    .filter(element => element[0] >=0 && element[1] >= 0)
+                    .map(element => element[0] + element[1])
+                    .forEach(element => console.log(`The turtle took ${element} steps.`));
+
+console.log(Result);
